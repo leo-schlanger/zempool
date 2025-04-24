efe87eb7fb5ac89b6c8bcdeaf69464f37d940a32
leo-schlanger <leoschlanger@gmail.com>
2025-04-24 11:56:34 -0300

    add group

--- commands/group.py
+++ commands/group.py
@@ -3,10 +3,12 @@ logger = logging.getLogger("group")
 logger.setLevel(logging.DEBUG)
 from discord import app_commands
 from .generate import GenerateCommand
+from .defi import DefiTopCommand
 from .help import HelpCommand
 
 class ZenPoolGroup(app_commands.Group):
     def __init__(self):
         super().__init__(name="zenpool", description="ZenPool commands")
         self.add_command(GenerateCommand())
+        self.add_command(DefiTopCommand())
         self.add_command(HelpCommand())
